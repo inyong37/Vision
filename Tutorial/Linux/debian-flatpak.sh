a4ae7c74481d3f3aa71695f43736ca12997c2b94
Inyong Hwang <inyong1020@gmail.com>
2023-03-30 12:03:10 +0900

    Update debian-flatpak.sh

--- Tutorial/Linux/debian-flatpak.sh
+++ Tutorial/Linux/debian-flatpak.sh
@@ -6,7 +6,7 @@
 # Command: "sh debian-flatpak.sh" with user authority.
 # Note: User has to keep in mind to enter password for sudo commands.
 
-echo -e "\e[0;34m========== SETTING FLATPAK ON DEBIAN ==========\e[0m"
+echo -e "\e[0;34m========== SETTING FLATPAK ON DEBIAN 11 ==========\e[0m"
 
 # VARIABLES
 echo -e "\e[0;32m---------- variables ----------\e[0m"
@@ -14,19 +14,16 @@ echo $HOME
 
 # INSTALLING PACKAGES
 echo -e "\e[0;32m---------- packages ----------\e[0m"
-sudo apt install -y 
-  flatpak flatpak-builder cargo libpq-dev postgresql postgresql-contrib pkg-config libssl-dev \
-  python3-aiohttp python3-pip \
-  git libcurl4-openssl-dev libsoup2.4-dev autoconf dracut libtool libglib2.0-dev \
-  libgpgme-dev bison liblzma-dev e2fslibs-dev libfuse-dev libsystemd-dev tree gtk-doc-tools \
-  gir1.2-ostree-1.0 libostree-dev
-pip3 install tenacity pyparsing
+# sudo apt install -y flatpak flatpak-builder cargo libpq-dev postgresql postgresql-contrib pkg-config libssl-dev python3-aiohttp python3-pip git libcurl4-openssl-dev libsoup2.4-dev autoconf dracut libtool libglib2.0-dev libgpgme-dev bison liblzma-dev e2fslibs-dev libfuse-dev libsystemd-dev tree gtk-doc-tools gir1.2-ostree-1.0 libostree-dev
+# pip3 install tenacity pyparsing
+sudo apt install -y git python3-aiohttp python3-pip autoconf gtk-doc-tools libglib2.0-dev libgpgme-dev libtool bison liblzma-dev e2fslibs-dev libcurl4-openssl-dev libsoup2.4-dev libfuse-dev
+pip3 install tenacity
 
 # INSTALLING OSTREE
-echo -e "\e[0;32m---------- ostree ----------\e[0m"
+echo -e "\e[0;32m---------- ppa-ostree ----------\e[0m"
 cd $HOME
-git clone https://github.com/ostreedev/ostree.git
-cd ostree
+git clone https://github.com/flatpak/ppa-ostree.git
+cd ppa-ostree
 git submodule update --init
 env NOCONFIGURE=1 ./autogen.sh
 ./configure --prefix=/usr --with-dracut --with-curl --with-builtin-grub2-mkconfig
