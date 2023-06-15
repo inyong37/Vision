763161a3eab76946e2a03a6f85a24060efe5c8a0
Inyong Hwang <inyong1020@gmail.com>
2023-06-15 16:19:20 +0900

    Update ubuntu-kubernetes.sh

--- Tutorial/Infrastructure/Kubernetes/Script/ubuntu-kubernetes.sh
+++ Tutorial/Infrastructure/Kubernetes/Script/ubuntu-kubernetes.sh
@@ -54,8 +54,8 @@ systemctl start crio
 # INSTALL KUBERNETES
 echo "========== INSTALL KUBERNETS =========="
 apt update && apt install -y apt-transport-https ca-certificates curl
-curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt update && apt install -y kubeadm=1.24.10-00 kubectl=1.24.10-00 kubelet=1.24.10-00
 apt-mark hold kubelet kubeadm kubectl
 
