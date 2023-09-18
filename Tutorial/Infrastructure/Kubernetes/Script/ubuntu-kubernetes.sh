c1c90d7d8a37a945ee5696416178ee3a4372aa69
Inyong Hwang <inyong1020@gmail.com>
2023-09-18 16:04:53 +0900

    Update ubuntu-kubernetes.sh

--- Tutorial/Infrastructure/Kubernetes/Script/ubuntu-kubernetes.sh
+++ Tutorial/Infrastructure/Kubernetes/Script/ubuntu-kubernetes.sh
@@ -52,7 +52,7 @@ systemctl enable crio
 systemctl start crio
 
 # INSTALL KUBERNETES
-echo "========== INSTALL KUBERNETS =========="
+echo "========== INSTALL KUBERNETES =========="
 apt update && apt install -y apt-transport-https ca-certificates curl
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
